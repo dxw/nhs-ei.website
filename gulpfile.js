fb5e598a5d1c29fa7ea1feaefe08dab7fb2d4c8e
Dragon (David McKee) <dragon@dxw.com>
2021-06-25 13:09:18 +0100

    Increase version of Gulp to 5 and other npm package wrangling.
    There were a number of security issues, and this felt surprisingly complicated.
    Firstly, node-sass was a dependency of Gulp 4 and required the version of
    Node to be pinned to 14 in order to install cleanly.
    
    Gulp 5 depends on (dart) sass or node-sass, but node-sass is deprecated:
    https://www.npmjs.com/package/node-sass so we are moving to (Dart) 'sass'.
    This probably removes the requirement to pin to version 14, but it's the LTS
    so I don't see a reason to change this. In addition to this change, Gulp 5
    will error out with an excellent error message to make the change shown in
    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const clean = require('gulp-clean');
 const webpack = require('webpack-stream');
 const package = require('./package.json');

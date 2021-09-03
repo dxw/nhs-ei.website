2d958a3750f5e59e1ba7f6788dcbbb84f18debb1
Dragon (David McKee) <dragon@dxw.com>
2021-09-03 17:49:01 +0100

    Visual Studio Code integration
    Based on the instructions at https://testdriven.io/blog/django-debugging-vs-code/
    Note that this currently uses ptvsd which is deprecated in favour of debugpy,
    but is useful as a proof of concept.
    
    Please read vscode.txt for additional information.
    
    Should only open service on port 3000 if running in DEBUG mode.

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.conf import settings
 
 if __name__ == "__main__":
+    if settings.DEBUG:
+        if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
+            import ptvsd
+
+            ptvsd.enable_attach(address=("0.0.0.0", 3000))
+            ptvsd.wait_for_attach()
+            print("Attached!")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cms.settings.dev")
 
     from django.core.management import execute_from_command_line

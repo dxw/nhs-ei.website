e9c53052e928fb4439ac4aa5cf9bec8356070a5b
Nick Jackson <nick@dxw.com>
2022-02-16 14:43:32 +0000

    Add promoted search to the admin backend
    This allows specific content to be added as promoted for a given search
    term.
    
    This commit does _not_ include front-end changes necessary to display
    promoted content.

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sitemaps",
+    "wagtail.contrib.search_promotions",
     "wagtailnhsukfrontend",
     "wagtailnhsukfrontend.settings",
     # importer

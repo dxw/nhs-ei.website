f55dcccb79398102d26288b73c7ff0278b45d592
Toby Batch <tobias@neontribe.co.uk>
2022-03-08 11:14:08 +0000

    wip: menus expand

--- packages/nhsuk.js
+++ packages/nhsuk.js
@@ -3,3 +3,4 @@ import '../node_modules/nhsuk-frontend/packages/nhsuk';
 import './search-toggle-fix'; //for issue https://github.com/rkhleics/nhs-ei.website/issues/29
 
 import './mega-menu-toggle';
+import './mega-menu-expander';

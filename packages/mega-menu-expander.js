308b491a24f597db98ecfccb619f6f6cce2914ab
Toby Batch <tobias@neontribe.co.uk>
2022-03-08 16:59:00 +0000

    feature: menu data is in the page

--- packages/mega-menu-expander.js
+++ packages/mega-menu-expander.js
@@ -1,21 +1,9 @@
-const showHideSubmenu = (submenuId, submenuDepth) => {
-    const menus = document.getElementsByClassName("toggle-menu");
-    console.log(submenuId, submenuDepth);
-    document.getElementById(submenuId).style.display = "block";
-    // debugger;
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleMenu = document.getElementsByClassName("toggle-menu");
+    const toggleMenu = document.getElementsByClassName("toggle-menu-expand");
 
     for (const link of toggleMenu) {
         link.addEventListener("click", (event) => {
-            console.log(event.target);
-            event.preventDefault();
-            const target = event.target;
-            const submenuId = target.getAttribute("data-submenu")
-            const submenuDepth = target.getAttribute("data-depth")
-            showHideSubmenu(submenuId, submenuDepth);
+            // Add listener here
         });
     }
 });

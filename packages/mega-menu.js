b6bf13c36d4c2eb6161057a6b9406603cb2b9dbb
Israt <isratchoudhury@gmail.com>
2022-03-16 15:22:42 +0000

    Styling the first level of the mega menu
    Add HTML and styling the primary nave into a grid of five columns, and full width at tablet and mobile
    for the developers to add functionality afterwards

--- packages/mega-menu.js
+++ packages/mega-menu.js
@@ -7,33 +7,11 @@ Templates: cms/templates/partials/megamenu
 /* ******************** Burger Menu ******************** */
 
 const show = function (elem) {
-    const getHeight = function () {
-        elem.style.display = 'block';
-        const height = elem.scrollHeight + 'px';
-        elem.style.display = '';
-        return height;
-    };
-
-    const height = getHeight();
     elem.classList.add('is-visible');
-    elem.style.height = height;
-
-    window.setTimeout(function () {
-        elem.style.height = '';
-    }, 350);
-
 };
 
 const hide = function (elem) {
-    elem.style.height = elem.scrollHeight + 'px';
-
-    window.setTimeout(function () {
-        elem.style.height = '0';
-    }, 1);
-
-    window.setTimeout(function () {
-        elem.classList.remove('is-visible');
-    }, 350);
+    elem.classList.remove('is-visible');
 };
 
 const toggle = function (elem, timing) {

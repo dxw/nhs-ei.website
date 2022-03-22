4f91f8ebf8ef9c80bd7c01f8bba31349b1b8b701
Toby Batch <tobias@neontribe.co.uk>
2022-03-22 10:42:07 +0000

    fix: url format for leaf pages

--- cms/browse/urls.py
+++ cms/browse/urls.py
@@ -5,5 +5,5 @@ from cms.browse import views
 urlpatterns = [
     path("", views.browse, name="browse"),
     path("<section>/", views.browse_branch, name="browse_branch"),
-    path("<section>/<branch>", views.browse_leaf, name="browse_leaf"),
+    path("<section>/<branch>/", views.browse_leaf, name="browse_leaf"),
 ]

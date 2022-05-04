858ce0e0c0ef5a97272f8bae63caec4e560c73ae
Chris Wright <chris@dxw.com>
2022-05-04 16:37:28 +0100

    Remove old deployment deirectory

--- deployment/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  project = "${var.prefix}-${terraform.workspace}"
-  default_tags = {
-    prefix : var.prefix,
-    managedby : "terraform",
-    project : "nhsei-website",
-    environment : terraform.workspace,
-  }
-}
